ab7c3ade4f97fd289c6617c327bd92c6528b8057
Roman Nikonov <code@nic11.xyz>
2026-05-21 21:45:00 +0200

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -25408,8 +25408,7 @@ async function gatherData(options) {
     baseSha,
     retries,
     concurrencyLimit,
-    primaryRepo,
-    globalToken
+    primaryRepo
   } = options;
   const limit = pLimit(concurrencyLimit);
   const MyOctokit = Octokit2.plugin(retry);
@@ -25426,7 +25425,7 @@ async function gatherData(options) {
   const mergeRequests = [];
   for (const repoConfig of repositories) {
     const { owner, repo, labels, token } = repoConfig;
-    const octokit = getOctokit(token || globalToken);
+    const octokit = getOctokit(token);
     console.log(`[gather] Repository: ${owner}/${repo}, Labels: ${labels.join(", ")}`);
     let foundItems = [];
     if (labels.length > 0) {
@@ -25641,6 +25640,8 @@ async function run() {
     for (const repository of repositories) {
       if (repository.token) {
         core.setSecret(repository.token);
+      } else {
+        repository.token = globalToken;
       }
     }
     let path = core.getInput("path");
@@ -25693,7 +25694,7 @@ async function run() {
       console.log(`[exact] Base SHA: ${commitTuple.baseSha}, PRs: ${commitTuple.prs.length}`);
     } else {
       const firstRepo = repositories[0];
-      const firstRemoteUrl = `https://x-access-token:${firstRepo.token || globalToken}@github.com/${firstRepo.owner}/${firstRepo.repo}.git`;
+      const firstRemoteUrl = `https://x-access-token:${firstRepo.token}@github.com/${firstRepo.owner}/${firstRepo.repo}.git`;
       console.log(`[gather] Setting remote to first repo to read base SHA`);
       await exec.exec("git remote set-url origin", [firstRemoteUrl], { cwd: path });
       await execWithRetry("git", ["fetch", "origin"], path, fetchRetries);
@@ -25704,8 +25705,7 @@ async function run() {
         baseSha,
         retries,
         concurrencyLimit,
-        primaryRepo: baseRepo || void 0,
-        globalToken
+        primaryRepo: baseRepo || void 0
       });
       commitTuple = gatherResult.commitTuple;
       console.log(`[gather] CommitTuple: ${commitTuple.toString()}`);
@@ -25723,7 +25723,7 @@ async function run() {
     }
     {
       const baseRepoConfig = baseRepo ? repositories.find((r) => `${r.owner}/${r.repo}` === baseRepo) ?? repositories[0] : repositories[0];
-      const verifyRemoteUrl = `https://x-access-token:${baseRepoConfig.token || globalToken}@github.com/${baseRepoConfig.owner}/${baseRepoConfig.repo}.git`;
+      const verifyRemoteUrl = `https://x-access-token:${baseRepoConfig.token}@github.com/${baseRepoConfig.owner}/${baseRepoConfig.repo}.git`;
       await exec.exec("git remote set-url origin", [verifyRemoteUrl], { cwd: path });
       await execWithRetry("git", ["fetch", "origin"], path, fetchRetries);
       const currentHead = await execStdout("git", ["rev-parse", "HEAD"], { cwd: path });
@@ -25751,7 +25751,7 @@ async function run() {
         continue;
       }
       console.log(`[!] Processing ${fullRepoName}: ${matchingPrs.length} PRs to merge`);
-      const remoteUrl = `https://x-access-token:${token || globalToken}@github.com/${owner}/${repo}.git`;
+      const remoteUrl = `https://x-access-token:${token}@github.com/${owner}/${repo}.git`;
       console.log(`[!] Setting remote origin URL to: https://x-access-token:***@github.com/${owner}/${repo}.git`);
       await exec.exec("git remote set-url origin", [remoteUrl], { cwd: path });
       console.log("[!] Fetching from new origin");
